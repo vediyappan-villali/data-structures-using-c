c66a8eb00338ea8cce1f9e340a7918cbef4c075b
vediyappan-villali <vediyappan.villali@gmail.com>
2022-04-28 08:48:37 +0530

    add program to passing array elements to a function

---
+++ arrays-pointers-structures/04_pass-array-elements-to-function.c
@@ -0,0 +1,39 @@
+/*
+ * Description: C program to illustrate passing array elements to a function.
+ *
+ * Author: Vediyappan Villali
+ * Courtesy: Suresh Kumar Srivastava, Deepali Srivastava
+ */
+
+#include <stdio.h>
+
+// function declaration
+void find_odd_even (int num);
+
+int main()
+{
+    int  arr[5];
+    int  i;
+
+    // getting elements of array
+    for (i=0; i<5; i++)
+    {
+        printf ("Enter value of arr[%d]: ", i);
+	scanf ("%d", &arr[i]);
+
+	// passing array elements to function
+	find_odd_even (arr[i]);
+
+	printf ("\n");
+    }
+
+    return 0;
+}
+
+void find_odd_even (int num)
+{
+    if (num % 2)
+        printf ("%d is an odd number", num);
+    else
+        printf ("%d is an even number", num);
+}
